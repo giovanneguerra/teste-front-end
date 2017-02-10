1f8cec4a56dee672d3ac8b5aa02dec41eb11749d
Giovanne Guerra <giovanneguerra@hotmail.com>
2017-02-10 09:57:34 -0200

    templating

--- app/application.js
+++ app/application.js
@@ -23,14 +23,86 @@ form.on('submit', function(e) {
   });
 });
 
-// fucking test
-var users = ['Alcest', 'Richard', 'Vincent'];
-ejs.render('<%= users.join(" | "); %>', {users: users},
-    {delimiter: '?'});
-console.log("começou");
-$.getJSON("teste.json", function(data){
-    var items = [];
-    $.each(data, function(k , v){
-        console.log(v[0]);
-    });
+// Templating
+
+$(function() {
+
+   var app = {
+
+       init: function() {
+           app.loadData();
+           app.filterChange();
+       },
+
+       loadData: function() {
+           $.getJSON('http://www.omdbapi.com/?t=alien&y=&plot=short&r=json').done(function(json) {
+               allData = json;
+               app.loadIndustryGrid(allData);
+           });
+       },
+
+       loadIndustryGrid: function() {
+           $.get('templates/template.html', function(template) {
+               var rendered = Mustache.render(template, allData);
+               $('#results').html(rendered);
+           });
+       },
+
+       filterChange: function() {
+           $('#filter-industry').change(function() {
+               var selectedIndustry = $(this).val();
+               app.applyFilter(selectedIndustry);
+           });
+       },
+
+       applyFilter: function(selectedIndustry) {
+           var $industry = $('#industry-grid-container').find('.industry');
+
+           $industry.each(function(i, val) {
+               var $industryData = $(this).data('industry');
+               if ($industryData.indexOf(selectedIndustry) >= 0 || selectedIndustry === '') {
+                   $(this).show();
+               } else {
+                   $(this).hide();
+               }
+           });
+           if ($.browser.chrome) {
+               $('.industry-container').masonry('layout');
+           }
+       },
+
+       filterChange2: function() {
+           $('#filter-industry2').change(function() {
+               var selectedIndustry2 = $(this).val();
+               app.applyFilter2(selectedIndustry2);
+           });
+       },
+
+       applyFilter2: function(selectedIndustry) {
+           var $industry = $('#industry-grid-container').find('.industry');
+
+           $industry.each(function(i, val) {
+               var $industryData = $(this).data('industry');
+               if ($industryData.indexOf(selectedIndustry) >= 0 || selectedIndustry === '') {
+                   $(this).show();
+               } else {
+                   $(this).hide();
+               }
+           });
+           if ($.browser.chrome) {
+               $('.industry-container').masonry('layout');
+           }
+       }
+
+   };
+
+   app.init();
+
+});
+
+//pagination
+$('.pagination').jqPagination({
+    paged: function(page) {
+        // do something with the page variable
+    }
 });
